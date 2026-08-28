f9d755cd2651af62a9c6ea34bdb1b257cdec9600
devanshigoyal3030-lgtm <devanshigoyal3030@gmail.com>
2026-08-29 01:46:51 +0530

    Time: 0 ms (100%), Space: 43.6 MB (98.04%) - LeetHub

--- 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.java
+++ 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.java
@@ -1,7 +1,7 @@
 class Solution {
     public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
         int count=0;
-        for(int i=0;i<hours.length;i++){
+        for(int i=0;i<=hours.length-1;i++){
             if((hours[i]==target)||(hours[i]>target)){
                 count++;
 
